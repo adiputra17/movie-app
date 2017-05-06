36467d42949dcb52e9c166c1faae7b95772d589a
adiputra17 <adiputrautama17@gmail.com>
2017-05-06 22:00:28 +0700

    Add Menu + Menu's Icon

--- app/src/main/java/com/example/adiputra/movie_app/ListActivity.java
+++ app/src/main/java/com/example/adiputra/movie_app/ListActivity.java
@@ -1,11 +1,25 @@
 package com.example.adiputra.movie_app;
 
+import android.content.Context;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.menu.MenuBuilder;
+import android.support.v7.view.menu.MenuPopupHelper;
+import android.support.v7.widget.PopupMenu;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class ListActivity extends AppCompatActivity {
+
+    ImageButton btnMenu;
+    private Context mContext;
+    private View mAnchor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,12 +28,41 @@ public class ListActivity extends AppCompatActivity {
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu){
-        super.onCreateOptionsMenu(menu);
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+        btnMenu = (ImageButton) findViewById(R.id.btnMenu);
+        btnMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MenuBuilder menuBuilder =new MenuBuilder(ListActivity.this);
+                MenuInflater inflater = new MenuInflater(ListActivity.this);
+                inflater.inflate(R.menu.main, menuBuilder);
+                MenuPopupHelper popup = new MenuPopupHelper(ListActivity.this, menuBuilder, v);
+                popup.setForceShowIcon(true);
+
+                menuBuilder.setCallback(new MenuBuilder.Callback() {
+                    @Override
+                    public boolean onMenuItemSelected(MenuBuilder menu, MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.one:
+                                Toast.makeText(ListActivity.this,"You Clicked : " + item.getTitle(),Toast.LENGTH_SHORT).show();
+                                return true;
+                            case R.id.two:
+                                Toast.makeText(ListActivity.this,"You Clicked : " + item.getTitle(),Toast.LENGTH_SHORT).show();
+                                return true;
+                            case R.id.three:
+                                Toast.makeText(ListActivity.this,"You Clicked : " + item.getTitle(),Toast.LENGTH_SHORT).show();
+                                return true;
+                            default:
+                                return false;
+                        }
+                    }
+                    @Override
+                    public void onMenuModeChange(MenuBuilder menu) {}
+                });
+
+                popup.show();
+            }
+        });
+
     }
 }
